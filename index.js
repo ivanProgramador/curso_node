6775d0657d829ac4a7dab03b09457abf5e2bb0d6
ivan <ivan-guita@hotmail.com>
2023-03-25 11:38:39 -0300

    servidor recebendo requissições om parametros e devolvendo a informação ao usuario

--- index.js
+++ index.js
@@ -24,6 +24,23 @@ app.get('/perfil',function(req,res){
 });
 
 
+//rota com parametro
+//se essa rota for chamada assim: "localhost:4000/ola " o navegador vai dizer que ela não existe 
+// porque se ela recebe um parametro e esse parametro ela tem que ser chamada asssim  "localhost:4000/ola/algumacoisa"
+
+
+app.get('/ola/:nome/:empresa', function(req,res){
+
+    //REQ => dados que o usuario envia para o servidor 
+    //RES => reposta que o usuario recebe do servidor 
+    //pegando o nome que o usuario enviou para o servidor 
+    var nome = req.params.nome; 
+    var empresa = req.params.empresa;  
+
+    res.send('<h1>Ola  ' + nome + ' do ' + empresa +'</h1>');
+});
+
+
 //usando a função listern que recebe o numero da por ta 4000
 // o segundo paramentro é uma função que diz oque acontece se o servidor for ligado 
 
