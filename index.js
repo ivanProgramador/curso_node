74a2030e120d8b83ecd765b5f169c22254530e55
ivan <ivan-guita@hotmail.com>
2023-03-25 10:33:00 -0300

    servidor basico rodando com duas rotas funcionais

--- index.js
+++ index.js
@@ -6,8 +6,26 @@ const express  = require('express');
 const app = express();
 
 
+
+// criando uma rota
+
+app.get('/',function(req,res){
+
+ //em absolutamente todos os casos uma rota tem que ter uma resposta para o usuario
+ //se não a requisição entra em loop e a pagina trava 
+
+    res.send('essa e a rota home');
+
+});
+
+app.get('/perfil',function(req,res){
+
+    res.send('essa e a rota perfil');
+});
+
+
 //usando a função listern que recebe o numero da por ta 4000
-// o segundparamentro é uma função que diz oque acontece se o servidor for ligado 
+// o segundo paramentro é uma função que diz oque acontece se o servidor for ligado 
 
 app.listen(4000, function(erro){
 
